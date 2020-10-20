def7b0d67c5f109f909c88191668199dbf34f8cd
Jefferson Costa <jefferson.costa@pdmfc.com>
2020-10-20 21:41:14 +0100

    Inserção dos cabeçalhos nos arquivos

--- extract.php
+++ extract.php
@@ -24,7 +24,7 @@ class CNPJFull
     {
         $this->findZipFiles();
         $this->readZipFiles();
-//        $this->compactFiles();
+        $this->compactFiles();
     }
 
     private function findZipFiles()
