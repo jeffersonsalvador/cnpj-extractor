d2e7630382bbfe33c71b947d610b2846a967add0
Jefferson Costa <jeffersonsalvador@gmail.com>
2021-06-23 23:32:26 +0200

    Migrations

---
+++ db/migrations/20210621201536_empresas.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class Empresas extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change(): void
+    {
+        $table = $this->table('empresas');
+        $table
+            ->addColumn('cnpj', 'string', ['limit' => 14])
+            ->addColumn('natureza_juridica', 'string', ['limit' => 2])
+            ->create();
+
+    }
+}
