776a4c8b7810f72dbb6f0cff9ad422761caf0c12
Jefferson Costa <jeffersonsalvador@gmail.com>
2023-12-09 09:08:09 +0100

    refactoring

--- src/lang/en/messages.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-return [
-    'command_description' => 'Processes CNPJ files in ZIP and inserts them into the database.'
-];
--- src/lang/pt_BR/messages.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-return [
-    'command_description' => 'Processa arquivos CNPJ em ZIP e insere no banco de dados.'
-];
